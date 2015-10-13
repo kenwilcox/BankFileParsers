3330454b21024d3150b8b080812b60d1fb3b1d4c
Ken Wilcox <wilcoxk@medamerica.com>
2015-10-13 15:46:48 -0600

    Created nuspec

--- BankFileParsers/Properties/AssemblyInfo.cs
+++ BankFileParsers/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BankFileParsers")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Utility methods and classes for parsing common bank files")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("kenwilcox")]
 [assembly: AssemblyProduct("BankFileParsers")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Ken Wilcox 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.1.0")]
