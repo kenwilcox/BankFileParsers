700419abab28666d0220cc8a5ae87e684b4577a5
Ken Wilcox <wilcoxk@gmail.com>
2022-01-07 11:39:04 -0700

    Added Change Log to readme Added .editor config Added License to package

--- BankFileParsers/pack.bat
+++ BankFileParsers/pack.bat
@@ -1,3 +1,5 @@
 REM del *.nupkg
-nuget pack BankFileParsers.csproj -Prop Configuration=Release
-nuget push BankFileParsers.0.1.6.nupkg -Source https://api.nuget.org/v3/index.json
+REM run the "Pack" option on the project in Visual Studio
+REM I do not know why the command line stopped working
+move bin\Release\BankFileParsers.0.1.8.nupkg .
+nuget push BankFileParsers.0.1.8.nupkg -Source https://api.nuget.org/v3/index.json
