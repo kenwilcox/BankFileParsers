a4a8c3b62476206c1ca3b00eabb4bb57da4c5aa1
Ken Wilcox <wilcoxk@gmail.com>
2022-08-22 18:29:54 -0600

    forgot this too

--- BankFileParsers/pack.bat
+++ BankFileParsers/pack.bat
@@ -1,5 +1,5 @@
 REM del *.nupkg
 REM run the "Pack" option on the project in Visual Studio
 REM I do not know why the command line stopped working
-move bin\Release\BankFileParsers.0.1.8.nupkg .
-nuget push BankFileParsers.0.1.8.nupkg -Source https://api.nuget.org/v3/index.json
+move bin\Release\BankFileParsers.0.1.9.nupkg .
+nuget push BankFileParsers.0.1.9.nupkg -Source https://api.nuget.org/v3/index.json
